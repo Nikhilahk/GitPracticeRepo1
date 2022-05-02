130e7c19251c3454e28182a1b952db3346ca7fca
Nikhila <vnaganikhila@dovercorp.com>
2022-05-02 05:24:33 +0000

    Commiting first file

---
+++ TestProject1/Script/Unit1.js
@@ -0,0 +1,25 @@
+﻿function calling_a_textFile()
+{
+  let FS = getActiveXObject("Scripting.FileSystemObject");
+
+  let F = FS.Open("C://Users//11036680//Documents//PythonScript//reading_excel.js");
+  var s=[];
+  while(! F.AtEndOfStream)
+  {
+    //Log.Message(F.ReadLine());
+   s.push(F.ReadLine());
+  }
+  return s;
+  F.Close();
+}
+
+
+  
+
+
+
+
+
+
+
+
